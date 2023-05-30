64455c372f37b169439622c769add4e56f624790
Константин Шперлинг <Puma-Yellow@yandex.ru>
2023-05-30 18:58:59 +0500

    Configure static files and add namespace

--- pylife/blog/urls.py
+++ pylife/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.index),
     path('posts/<slug>/', views.get_post),
--- pylife/pylife/urls.py
+++ pylife/pylife/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('blog.urls')),
+    path('', include('blog.urls', namespace='blog')),
     path('admin/', admin.site.urls),
 ]
