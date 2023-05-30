393fb9d00bb9e58a287f113e77bb0a694a57812e
Константин Шперлинг <Puma-Yellow@yandex.ru>
2023-05-30 19:37:53 +0500

    Create models: Post, Comment. Add them to admin panel

--- pylife/blog/admin.py
+++ pylife/blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created', 'status',)
+    prepopulated_fields = {'slug': ('title',), }
+    search_fields = ('body',)
+    list_filter = ('created', 'status',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created',)
+    search_fields = ('body',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- pylife/blog/models.py
+++ pylife/blog/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+        ('hidden', 'Hidden')
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='draft'
+    )
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'Post',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.author, self.post.title)
