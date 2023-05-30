c5243feac10c68e34b7b135e14aac6d3ef4f3be4
Константин Шперлинг <Puma-Yellow@yandex.ru>
2023-05-31 01:09:56 +0500

    Implemented function to display posts on homepage and fixed HTML template

--- pylife/blog/views.py
+++ pylife/blog/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post
+
 
 def index(request):
-    template = 'blog/index.html'
-    return render(request, template)
+    posts = Post.objects.order_by('-created')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/index.html', context)
 
 
 def get_post(request, slug):
--- pylife/pylife/settings.py
+++ pylife/pylife/settings.py
@@ -2,23 +2,14 @@ import os
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-vxuk6x)93x9v7!(+am_s^o7b1sfv&wf)5wj&sm_529pygklp_u'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
@@ -109,10 +100,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
