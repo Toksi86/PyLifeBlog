cee0d1c24cc147ecbe313bbfb2d63bb90527bdae
Константин Шперлинг <Puma-Yellow@yandex.ru>
2023-05-31 13:42:02 +0500

    Add app users, template login, logout, urlpatterns for users, fixed static

--- pylife/pylife/urls.py
+++ pylife/pylife/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
     path('', include('blog.urls', namespace='blog')),
     path('admin/', admin.site.urls),
 ]
