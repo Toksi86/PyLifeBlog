84d5d93a075d3418729a513b8877af9bf8180252
Константин Шперлинг <Puma-Yellow@yandex.ru>
2023-06-19 19:01:17 +0500

    Add custom handler 403, 404, 500

--- pylife/pylife/urls.py
+++ pylife/pylife/urls.py
@@ -9,3 +9,7 @@ urlpatterns = [
     path('', include('blog.urls', namespace='blog')),
     path('about/', include('about.urls', namespace='about')),
 ]
+
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+handler403 = 'core.views.permission_denied'
